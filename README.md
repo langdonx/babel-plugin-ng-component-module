63043e9d3fb1ca742ad125c21a8f9c2ffcf07bbc
Langdon Oliver <langdonx@protossecurity.com>
2017-02-13 23:42:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# About
+
+I don't really want to write `import`, `export`, `angular.module`, and `angular.component` statements all day long. This plugin automatically generates those statements after inspecting the filesystem at compile time.
+
+It's being used in my [angular-2017-starter repo](https://github.com/langdonx/angularjs-2017-starter), where my components directory contains logically grouped components together that are lazy loaded on demand at runtime.
+
+Given the following directory structure:
+
+    src/components/todo/
+    -------------------/index.js
+    -------------------/todo/
+    ------------------------/todo.html
+    ------------------------/todo.js
+    ------------------------/todo.scss
+    src/components/todo/todo-list/
+    -----------------------------/todo-list.html
+    -----------------------------/todo-list.js
+    -----------------------------/todo-list.scss
+
+Using `'ngComponentModule'` inside `todo/index.js` will generate the following code prior to compilation:
+
+    import todo from './todo/todo'
+    import todoList from './todo-list/todo-list'
+    var todoModule = angular.module('app.todo', [])
+	todoModule.component(todo.name, todo)
+	todoModule.component(todoList.name, todoList)
+
+# To Do
+- Add some elaborate integration test
+- Allow for root namespace to be configured (perhaps `'ngModelComponent:app'`)
+- Maybe allow a comment (`/* ngComponentModule */`) in lieu of a directive
+- Maybe figure out how to add semicolons even though they're not necessary
+- Maybe figure out how to chain `.component()` calls off of `.module()` (hint: loop)
---
+++ src/index.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = ({ types }) => ({
+  visitor: {
+    Program(programPath, state) {
+      const directiveIndex = programPath.node.directives.findIndex(d => d.value.value === 'ngComponentModule');
+
+      // if the ngComponentModule directive is being used
+      if (directiveIndex > -1) {
+        // remove the directive from the source
+        programPath.node.directives.splice(directiveIndex, 1);
+
+        // iterate sub-directories to build a list of necessary imports
+        const components = [];
+        const componentRootDir = path.dirname(state.file.opts.filename);
+
+        fs.readdirSync(componentRootDir)
+          .filter(name => fs.statSync(path.join(componentRootDir, name)).isDirectory())
+          .forEach((componentSubDir) => {
+            const name = path.basename(componentSubDir);
+
+            if (fs.existsSync(path.join(componentRootDir, componentSubDir, 'index.js')) === true) {
+              components.push({ name, path: `./${name}` });
+            }
+            else if (fs.existsSync(path.join(componentRootDir, componentSubDir, `${name}.js`)) === true) {
+              components.push({ name, path: `./${name}/${name}` });
+            }
+            else if (fs.existsSync(path.join(componentRootDir, componentSubDir, `${name}.component.js`)) === true) {
+              components.push({ name, path: `./${name}/${name}.component` });
+            }
+            else {
+              console.warn('[babel-plugin-ng-component-module WARNING] Skipping module directory, cannot find appropriately named default file', componentSubDir);
+            }
+          });
+
+        // create code!
+        const componentRootDirName = path.basename(componentRootDir);
+        const moduleVar = `${componentRootDirName}Module`;
+
+        // create import statements for each component
+        components.forEach((component) => {
+          programPath.pushContainer(
+            'body',
+            types.importDeclaration(
+              [
+                types.importDefaultSpecifier(
+                  types.identifier(component.name)
+                )
+              ],
+              types.stringLiteral(component.path)
+            )
+          );
+        });
+
+        // create an angular module
+        programPath.pushContainer(
+          'body',
+          types.variableDeclaration(
+            'const', [
+              types.VariableDeclarator(
+                types.identifier(moduleVar),
+                types.callExpression(
+                  types.memberExpression(
+                    types.identifier('angular'),
+                    types.identifier('module'),
+                    false
+                  ), [
+                    types.stringLiteral(`app.${componentRootDirName}`),
+                    types.arrayExpression()
+                  ]
+                )
+              )
+            ]
+          )
+        );
+
+        // create angular components for each component
+        components.forEach((component) => {
+          programPath.pushContainer(
+            'body',
+            types.callExpression(
+              types.memberExpression(
+                types.identifier(moduleVar),
+                types.identifier('component')
+              ), [
+                types.memberExpression(
+                  types.identifier(component.name),
+                  types.identifier('name')
+                ),
+                types.identifier(component.name)
+              ]
+            )
+          );
+        });
+      }
+    }
+  }
+});
